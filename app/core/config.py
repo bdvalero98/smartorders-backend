68536f89af52c15fa327e46ab64d0c5807dcbfed
bdvalero98 <danilovalero25@gmail.com>
2025-06-14 16:34:40 -0500

    Organiza estructura del proyecto y añade configuración con archivo .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,9 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "SmartOrders API"
     VERSION: str = "1.0.0"
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite:///./test.db"
 
     class Config:
         env_file = ".env"
