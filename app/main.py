6ba72939a4234630b31bde634e31a2cd408d5721
bdvalero98 <danilovalero25@gmail.com>
2025-06-14 20:52:39 -0500

    Middleware global para manejo de errores y logging

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.api.v1 import routes_producto
 from app.api.v1 import routes_root
 from app.core.config import settings
+from app.middleware.error_handler import ErrorHandlerMiddleware
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -10,5 +11,7 @@ app = FastAPI(
 )
 
 # Incluir rutas
-app.include_router(routes_root.router)
+app.add_middleware(ErrorHandlerMiddleware)
+
+app.include_router(routes_root.router, prefix="/api/v1")
 app.include_router(routes_producto.router, prefix="/api/v1")
--- app/middleware/error_handler.py
+++ app/middleware/error_handler.py
@@ -1,5 +1,42 @@
 import logging
+
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from starlette.middleware.base import BaseHTTPMiddleware
 from sqlalchemy.exc import SQLAlchemyError
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from app.exceptions.database import DatabaseException
+
+logger = logging.getLogger("smartorders")
+logging.basicConfig(level=logging.INFO)
+
+
+class ErrorHandlerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            response = await call_next(request)
+            return response
+        except DatabaseException as db_exc:
+            logger.error(f"Database error: {str(db_exc)}")
+            return JSONResponse(
+                status_code=500,
+                content={
+                    "detail": "Error interno de base de datos"
+                },
+            )
+        except SQLAlchemyError as sa_exc:
+            logger.error(f"SQLAlchemy error: {str(sa_exc)}")
+            return JSONResponse(
+                status_code=500,
+                content={
+                    "detail": "Error de base de datos"
+                },
+            )
+        except Exception as exc:
+            logger.error(f"Unhaled exception: {str(exc)}")
+            return JSONResponse(
+                status_code=500,
+                content={
+                    "detail": "Error interno del servidor"
+                },
+            )
